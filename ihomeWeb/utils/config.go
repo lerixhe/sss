7784568ad18671a1a72f4e7623915860d4f2cc54
Lerix <185734549@qq.com>
2019-09-17 15:25:21 +0800

    添加项目笔记，配置文件修复

--- ihomeWeb/utils/config.go
+++ ihomeWeb/utils/config.go
@@ -22,7 +22,7 @@ var (
 
 func InitConfig() {
 	//从配置文件读取配置
-	appconf, err := config.NewConfig("ini", "./conf/app.conf")
+	appconf, err := config.NewConfig("ini", "../ihomeWeb/conf/app.conf")
 	if err != nil {
 		beego.Debug(err)
 		return
